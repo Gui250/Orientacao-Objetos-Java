12e492419f0c6d7e9719f7520f2b3ea89927bd1e
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-12-03 08:40:33 -0300

    primeiro commit

---
+++ src/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+   public int ano;
+   public String modelo;
+   public String marca;
+   public String cor;
+
+   public Carro(String modelo, String marca, int ano, String cor) {
+      this.modelo = modelo;
+      this.marca = marca;
+      this.ano = ano;
+      this.cor = cor;
+   }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro fusca = new Carro("Fusca", "Volkswagen", 1978, "preto");
+
+    }
+}
