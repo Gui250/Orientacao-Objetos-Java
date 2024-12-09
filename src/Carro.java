da12e18e5e9db8704a53bf4309e25734f4923f36
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-12-09 14:22:13 -0300

    estudando métodos

--- src/Carro.java
+++ src/Carro.java
@@ -3,11 +3,47 @@ public class Carro {
    public String modelo;
    public String marca;
    public String cor;
+   public boolean ligado;
+   public int aceleracao;
 
    public Carro(String modelo, String marca, int ano, String cor) {
       this.modelo = modelo;
       this.marca = marca;
       this.ano = ano;
       this.cor = cor;
+      this.ligado = false;
+      this.aceleracao = 0;
    }
+
+   public void ligar() {
+      if(this.ligado) {
+         return;
+      }
+      this.ligado = true;
+   }
+
+   public void desligar() {
+      if(!this.ligado) {
+         return;
+      }
+      this.aceleracao = 0;
+      this.ligado = false;
+   }
+
+   public void acelerar(String tipoAceleracao) {
+         switch(tipoAceleracao) {
+            case "forte":
+               this.aceleracao += 1000;
+               break;
+            case "fraco":
+               this.aceleracao += 500;
+               break;
+         }
+   }
+
+
+   public void acelerar() {
+         this.aceleracao += 750;
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Carro fusca = new Carro("Fusca", "Volkswagen", 1978, "preto");
+        System.out.println(fusca.ligado);
+        Carro gol = new Carro("Gol", "Volkswagen", 1990, "branco");
 
+        System.out.println(gol.ligado);
+        gol.acelerar("forte");
+        gol.acelerar("fraco");
+
+        System.out.println(gol.aceleracao);
     }
 }
